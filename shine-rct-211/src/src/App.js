7ecaf35fb885e09b30ab03b153a79072ead0a976
Siva  gurubilli <92457968+sivagurubilli@users.noreply.github.com>
2022-07-20 19:42:31 +0530

    register

---
+++ shine-rct-211/src/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Routesall from './routes/Routes';
+
+function App() {
+  return (
+    <div className="App">
+ <Routesall />
+    </div>
+  );
+}
+
+export default App;
