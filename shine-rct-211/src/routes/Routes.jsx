e98ba3eb72e38ea9b8d057e20a1155e35dd00cba
Siva  gurubilli <92457968+sivagurubilli@users.noreply.github.com>
2022-07-20 19:47:10 +0530

    register pages

---
+++ shine-rct-211/src/routes/Routes.jsx
@@ -0,0 +1,26 @@
+import {Routes,Route} from "react-router-dom"
+import Home from "../components/Home"
+import Login from "../components/login/Login"
+import Register from "../components/register/Register"
+
+
+
+const Routesall =()=>{
+    return(
+<div>
+    <Routes >
+        <Route path="/" element={<Home /> }>
+
+        </Route>
+<Route path ="/login" element={<Login />}>
+
+</Route>
+<Route path="/register" element={<Register/>}></Route>
+    </Routes>
+</div>
+
+
+    )
+}
+
+export default Routesall
