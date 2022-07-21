be0ad5c9c219e8a862318e77864b43145e507aa1
Balu Ram Kumawat <26851509+baluramk6@users.noreply.github.com>
2022-07-21 21:00:44 +0530

    Course page

---
+++ src/components/Courses/SalesAndMarketing/Course/Courses.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Filter from "../Filter/Filter";
+import Course from "./Course";
+
+const Courses = (props) => {
+  return (
+    <div>
+      <Filter />
+      <Course />
+    </div>
+  );
+};
+
+export default Courses;
---
+++ src/components/Courses/SalesAndMarketing/SalesAndMarketing.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Courses from "./Course/Courses";
+
+function SalesAndMarketing(props) {
+  return (
+    <div>
+      <Courses />
+    </div>
+  );
+}
+
+export default SalesAndMarketing;
