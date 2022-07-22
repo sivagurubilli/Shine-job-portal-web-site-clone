61e04e6a3f74db520f59dcfa6c13b1264cf6662b
sivagurubilli <gurusrija123@gmail.com>
2022-07-22 21:14:25 +0530

    backend

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+
+const express = require("express")
+const mongoose = require("mongoose")
+const port = process.env.PORT || 5000;
+
+
+const cors = require("cors")
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+mongoose.connect("mongodb+srv://gurubilli:gurubilli@cluster0.dlpod.mongodb.net/shine-web",
+(err)=>{
+    if(err)return console.log(err);
+    console.log("connected to mongodb")
+})
+
+const Defaultdata = require("./Defaultdata")
+app.use("/reg",require("./routers/userRoutes"))
+app.use("/log",require("./routers/loginRouters"))
+
+app.listen(port,()=>
+console.log(`server start on port ${port}`))
+
+
+Defaultdata()
